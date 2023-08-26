63c6e00774a271cda2c25594b0ded15511d4b293
Elias de Almeida <eliasombr@gmail.com>
2023-08-25 23:31:18 -0300

    Renomeando diretório dos exercícios do slide 01

---
+++ 01_introduction/04_count_digit.py
@@ -0,0 +1,9 @@
+def countDigits(number):
+    return 1 if number < 10 else 1 + countDigits(number // 10)
+
+
+try:
+    number = int(input('>>> Informe um número: '))
+    print(f'O número {number} tem {countDigits(number)} dígitos')
+except Exception as exception:
+    print(exception)
