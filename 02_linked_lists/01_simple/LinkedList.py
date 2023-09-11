7791c66bf93417d977a3d29df0395a6b55b973c0
Elias de Almeida <eliasombr@gmail.com>
2023-09-11 19:03:16 -0300

    Adicionando novos métodos da classe LinkedList

--- 02_linked_lists/01_simple/LinkedList.py
+++ 02_linked_lists/01_simple/LinkedList.py
@@ -96,11 +96,66 @@ class LinkedList():
             else:
                 previous = current
             current = current.next
+    
+    def removeRepetitions(self):
+        current = self.head
+        while current.next:
+            if current.data == current.next.data:
+                current.next = current.next.next
+            else:
+                current = current.next
+    
+    def reverseInPairs(self):
+        current = self.head
+        while current and current.next:
+            currentCopy = current.data
+            current.data = current.next.data
+            current.next.data = currentCopy
+            current = current.next.next
+
+    def reverse(self):
+        if self.length == 1:
+            return
+        current = self.head
+        previous = None
+        while current.next:
+            next = current.next
+            current.next = previous
+            previous = current
+            current = next
+        current.next = previous
+        self.head = current
+    
+    def reverseAtGivenElement(self, current):
+        reverseList = LinkedList()
+        while current:
+            reverseList.insertAtBeginning(current.data)
+            current = current.next
+        return reverseList.head
+
+    def isPalindrome(self):
+        if self.head == None:
+            return False
+        if self.length == 1:
+            return True
+        current = firstHalf = self.head
+        for _ in range(self.length // 2):
+            current = current.next
+        if self.length % 2:
+            secondHalf = self.reverseAtGivenElement(current.next)
+        else:
+            secondHalf = self.reverseAtGivenElement(current)
+        while secondHalf:
+            if firstHalf.data != secondHalf.data:
+                return False
+            firstHalf = firstHalf.next
+            secondHalf = secondHalf.next
+        return True
 
     def printElements(self):
         position = 0
         current = self.head
-        while current:
+        while position != self.length:
             print(f'{position} - {current.data}')
             position += 1
             current = current.next
